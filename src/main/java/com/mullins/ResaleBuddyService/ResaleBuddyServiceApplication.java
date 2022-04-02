0e7cfb3f18d425b3e257ff9827deda294fd94480
Jared Mullins <jwmullins92@gmail.com>
2022-04-02 15:52:43 -0500

    first commit

---
+++ src/main/java/com/mullins/ResaleBuddyService/ResaleBuddyServiceApplication.java
@@ -0,0 +1,13 @@
+package com.mullins.ResaleBuddyService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResaleBuddyServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ResaleBuddyServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mullins/ResaleBuddyService/ResaleBuddyServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mullins.ResaleBuddyService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResaleBuddyServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
